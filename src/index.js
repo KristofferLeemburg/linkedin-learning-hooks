8aba22aec3551cfab61b6166607d24195364981b
leemburg <kristofferleemburg@gmail.com>
2021-04-12 15:23:01 +0200

    Dispatching actions with useReducer

--- src/index.js
+++ src/index.js
@@ -2,19 +2,35 @@ import React, {useReducer} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const initialState ={
+  message:"hii"
+}
+
+function reducer(state, action){
+  switch(action.type){
+    case "yell":
+      return{
+        message: `HEY! I JUST SAID ${state.message}`
+      }
+      case "whisper":
+        return{
+          message: `excuse me, I JUST SAID ${state.message}`
+        }
+  }
+}
+
 function App(){
-  const [checked, toggle] = useReducer(
-    (checked) => !checked , 
-    false
+  const [state, dispatch] = useReducer(
+    reducer , 
+    initialState
     )
- return <>
- <input
- type="checkbox"
- value={checked}
- onChange={toggle}
- />
- {checked ? "checked" : "not checked"}
-</>
+
+ return( <>
+<p>Message: {state.message}</p>
+<button onClick={() => dispatch({ type: "yell"})}>Yell</button>
+<button onClick={() => dispatch({ type: "whisper"})}>whisper</button>
+
+</>)
 }
 
 
